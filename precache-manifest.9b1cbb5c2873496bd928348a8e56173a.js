21fe244578a763e28329639596473f96b7896410
Surya Khandavilli <Khandavillisurya@gmail.com>
2020-03-07 22:58:43 +0530

    Updates

---
+++ precache-manifest.9b1cbb5c2873496bd928348a8e56173a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59b871f13c1cfbed88ef247f2b0695a",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/index.html"
+  },
+  {
+    "revision": "12039a1b6de94fefbbd6",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/css/main.c40e6b93.chunk.css"
+  },
+  {
+    "revision": "35425d848ed28e9af5f2",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/js/2.bc7a43c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/js/2.bc7a43c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12039a1b6de94fefbbd6",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/js/main.a0574c1d.chunk.js"
+  },
+  {
+    "revision": "d7283a4e778cdb83496b",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/js/runtime-main.9e3f5942.js"
+  },
+  {
+    "revision": "5f732fc63f45a9bad86b35e19f25967e",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/Billgenerator.5f732fc6.png"
+  },
+  {
+    "revision": "8f8e97a061cdfc5512f449cf85fab6a7",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/GoogleTimer.8f8e97a0.png"
+  },
+  {
+    "revision": "9920b2f97e3923821bc052b42f8bb81c",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/Marksrecorder.9920b2f9.png"
+  },
+  {
+    "revision": "cbb3bf7d1da0442e05877f5e3e2811f9",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/OMDB.cbb3bf7d.png"
+  },
+  {
+    "revision": "d861c973fc635bf604a2f41139a300cd",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/Todo.d861c973.png"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/bootstrapimg.71e42b5a.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "8ec9bacf8bc78b8ad161b5e9a2a16e87",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/jquery.8ec9bacf.svg"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "e36d17c992f39c9be81d9b03e8651665",
+    "url": "/suryakhandavilli/suryakhandavilli.github.io.git/static/media/web_developer.e36d17c9.png"
+  }
+]);
